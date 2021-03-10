9918b1a67fc1e759c06a165a3a6ebf264ce65d4e
ayumitanaka <tanakayumi13@gmail.com>
2021-03-10 19:33:04 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import M from 'materialize-css';
+import axios from 'axios'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './Header';
+import Modal from './Modal';
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      temp: '',
+      cityName: '',
+      weather: '',
+      high: '',
+      low: '',
+      icon: '',
+      showModal: false,
+    }
+  }
+  // GET API
+  // Do not put in render!! 
+  componentDidMount=()=>{
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=Vancouver&units=metric&appid=${process.env.REACT_APP_WEATHER_API}`
+    // console.log(url)
+    axios.get(url).then((response) => {
+      this.setState({
+        temp: response.data.main.temp,
+        cityName: response.data.name,
+        weather: response.data.decription,
+        high: response.data.main.temp_max,
+        low: response.data.main.temp_min,
+        icon: response.data.icon,
+      })
+    })
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  }
+
+  render () {
+    const iconUrl = `http://openweathermap.org/img/w/${this.state.icon}.png`
+
+    return (
+      <div className="App">
+        <Header cityName={this.state.cityName} temp={this.state.temp} />
+        <Modal
+          icon={iconUrl}       
+          cityName={this.state.cityName}
+          high={this.state.high}
+          low={this.state.low}
+          weather={this.state.weather}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Modal.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Modal = ({icon, cityName, high, low, weather}) => {
+
+    return (
+        <>
+            <button data-target='modal1' className='btn modal-trigger'>
+                Modal
+            </button>
+            <div id='modal1' className='modal'>
+            <div className='modal-content'>
+                <h4>{cityName}</h4>
+                <p>
+                High: {high} - Low: {low}
+                </p>
+                <p>
+                {weather} <img src={icon} alt='icon' />
+                </p>
+            </div>
+            <div className='modal-footer'>
+                <a
+                href='#!'
+                className='modal-close waves-effect waves-green btn-flat'
+                >
+                Close
+                </a>
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default Modal
