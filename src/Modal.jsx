49322b8679e44254cbb8c1dff25ede49e56fbdc9
ayumitanaka <tanakayumi13@gmail.com>
2021-03-11 00:17:23 +0000

    modified Modal.jsx

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -8,23 +8,23 @@ const Modal = ({icon, cityName, high, low, weather}) => {
                 Modal
             </button>
             <div id='modal1' className='modal'>
-            <div className='modal-content'>
-                <h4>{cityName}</h4>
-                <p>
-                High: {high} - Low: {low}
-                </p>
-                <p>
-                {weather} <img src={icon} alt='icon' />
-                </p>
-            </div>
-            <div className='modal-footer'>
-                <a
-                href='#!'
-                className='modal-close waves-effect waves-green btn-flat'
-                >
-                Close
-                </a>
-            </div>
+                <div className='modal-content'>
+                    <h4>{cityName}</h4>
+                    <p>
+                    High: {high} - Low: {low}
+                    </p>
+                    <p>
+                    {weather} <img src={icon} alt='icon' />
+                    </p>
+                </div>
+                <div className='modal-footer'>
+                    <a
+                    href='#!'
+                    className='modal-close waves-effect waves-green btn-flat'
+                    >
+                    Close
+                    </a>
+                </div>
             </div>
         </>
     )
